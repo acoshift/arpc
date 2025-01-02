a32074899462bf1e94a789e2f8b79fc00204d60b
Thanatat Tamtan <acoshift@gmail.com>
2025-01-02 12:07:09 +0700

    sse: impl WriteEvent, WriteData

--- sse.go
+++ sse.go
@@ -1,10 +1,15 @@
 package arpc
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 type SSEResponseWriter interface {
 	http.ResponseWriter
 	http.Flusher
+	WriteEvent(event, data string) error
+	WriteData(data string) error
 }
 
 var _ SSEResponseWriter = (*sseResponseWriter)(nil)
@@ -41,6 +46,31 @@ func (w *sseResponseWriter) WriteHeader(statusCode int) {
 func (w *sseResponseWriter) Write(b []byte) (int, error) {
 	w.writeHeader()
 	return w.w.Write(b)
+}
+
+func (w *sseResponseWriter) writeData(data string) error {
+	for _, x := range strings.Split(data, "\n") {
+		_, err := w.w.Write([]byte("data: " + x + "\n"))
+		if err != nil {
+			return err
+		}
+	}
+	_, err := w.w.Write([]byte("\n"))
+	return err
+}
+
+func (w *sseResponseWriter) WriteEvent(event, data string) error {
+	w.writeHeader()
+	_, err := w.w.Write([]byte("event: " + event + "\n"))
+	if err != nil {
+		return err
+	}
+	return w.writeData(data)
+}
+
+func (w *sseResponseWriter) WriteData(data string) error {
+	w.writeHeader()
+	return w.writeData(data)
 }
 
 func (w *sseResponseWriter) Flush() {
